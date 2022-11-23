2efee94c37248ed34aa536373a7d5f71fc598869
pranto <prantochandrodas@gmail.com>
2022-11-23 23:37:23 +0600

    first commit

---
+++ src/Pages/Header/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderNavbar from '../HeaderNavbar/HeaderNavbar';
+
+const Header = () => {
+    return (
+        <div>
+            <HeaderNavbar></HeaderNavbar>
+        </div>
+    );
+};
+
+export default Header;
